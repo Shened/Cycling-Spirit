7de219260891b4ab04afef6192589dbb68035576
Shened <joao.psm98@gmail.com>
2026-03-18 23:53:14 +0000

    secureCookie

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,11 @@ import { NextRequest, NextResponse } from "next/server";
 import { getToken } from "next-auth/jwt";
 
 export async function middleware(req: NextRequest) {
-  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+  const token = await getToken({
+    req,
+    secret: process.env.NEXTAUTH_SECRET,
+    secureCookie: process.env.NODE_ENV === "production",
+  });
   const isLoggedIn = !!token;
 
   const { pathname } = req.nextUrl;
